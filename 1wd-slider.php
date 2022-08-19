82270c3a4491949df69cbbfba8a9dad55da9de1c
upstar7 <upstar77@outlook.com>
2022-08-19 13:43:15 +0300

    1wd-slider

---
+++ 1wd-slider.php
@@ -0,0 +1,16 @@
+<?php
+/*
+    Plugin Name: iWD Slider
+    Plugin URI: https://1stwebdesigner.com/
+    Description: Slider Comment for WordPress
+    Version: 1.0
+    Author: Tony Yuan
+    Author URI: https://istwebdesigner.com/
+    License: GPLv2 or later
+*/
+
+    function fwds_slider_activation() {
+    }
+    register_activation_hook(__FILE__, 'fwds_slider_activation');
+    
+?>
