51e580149333198f5c84fb50ebcda139c1e944cb
upstar7 <upstar77@outlook.com>
2022-08-20 13:57:01 +0300

    change

--- 1wd-slider.php
+++ 1wd-slider.php
@@ -12,5 +12,13 @@
     function fwds_slider_activation() {
     }
     register_activation_hook(__FILE__, 'fwds_slider_activation');
-    
+    function fwds_slider_deactivation() {
+    }
+    register_activation_hook(__FILE__, 'fwds_slider_deactivation');
+
+    add_action( 'wp_enqueue_scripts', 'fwds_scripts');
+    function my_custom_function() {
+        wp_enqueue_script('jquery');
+    }
+    add_action('', 'my_custom_function')
 ?>
